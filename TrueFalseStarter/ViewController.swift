3de8be473039f6e7f17493b64aa65d0d5a3999c3
Ty Schenk <tyschenk2@me.com>
2016-08-26 01:04:16 -0400

    updated comments

--- TrueFalseStarter/ViewController.swift
+++ TrueFalseStarter/ViewController.swift
@@ -37,7 +37,7 @@ class ViewController: UIViewController {
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated
+        // Dispose of any resources that can be recreated.
     }
     
     func displayQuestion() {
